fecfa29777751d170732f866b6c3ebf9a34b4346
qrj <myused@sina.com>
2023-01-12 15:43:46 +0800

    test: release-it

--- scripts/bin/release.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process')
-const { join } = require('path')
-const { rootPath } = require('../config/path.config')
-
-function main() {
-  exec('node ' + join(rootPath, 'node_modules/release-it/bin/release-it'))
-}
-
-main()
