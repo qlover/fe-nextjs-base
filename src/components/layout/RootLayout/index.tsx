20c8b6a9df00e0d17a4ea009fcfd084f15f83a23
qrj <myused@sina.com>
2023-02-20 11:31:31 +0800

    feat: rootLayout seoProps

--- src/components/layout/RootLayout/index.tsx
+++ src/components/layout/RootLayout/index.tsx
@@ -1,20 +1,22 @@
 import { PageFooter, PageHeader } from '@/components/pages'
 import { PageHeaderProps } from '@/components/pages/PageHeader'
-import PageSeo from '@/components/pages/PageSeo'
+import PageSeo, { PageSeoProps } from '@/components/pages/PageSeo'
 import { FC } from 'react'
 
 type RootLayoutProps = {
   headerProps?: PageHeaderProps
+  seoProps?: PageSeoProps
 } & Component.UI
 
 const RootLayout: FC<Component.WithChildren<RootLayoutProps>> = ({
   headerProps,
   className,
+  seoProps,
   children
 }) => {
   return (
     <div className={className} data-root="RootLayout">
-      <PageSeo />
+      <PageSeo {...seoProps} />
 
       <PageHeader {...headerProps} />
 
